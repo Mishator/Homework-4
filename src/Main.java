2f128dbd15dd25a5ea653def99e58b64b5787a14
Mishator <voyteh89@mail.ru>
2023-02-25 23:18:06 +0300

    Выполнил Задача 1, объявил шесть типов переменных.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+}
+public static void task1 () {
+    System.out.println("Задача 1");
+    byte a = 100;
+    int b = 567980;
+    short c = 12780;
+    long d = 1659345L;
+    float e = 1.5678F;
+    double f = 3.2537825679;
+    System.out.println("byte a = " + a);
+    System.out.println("int b = " + b);
+    System.out.println("short c = " + c);
+    System.out.println("long d = " + d);
+    System.out.println("float e = " + e);
+    System.out.println("double f = " + f);
     }
 }
