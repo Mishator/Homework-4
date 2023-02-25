81070078c3df70a53e728aac3a20518a49cf630c
Mishator <voyteh89@mail.ru>
2023-02-26 00:16:38 +0300

    Выполнил Задача 2, инициализировал переменные.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
 }
 public static void task1 () {
@@ -17,5 +18,22 @@ public static void task1 () {
     System.out.println("long d = " + d);
     System.out.println("float e = " + e);
     System.out.println("double f = " + f);
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        float a = 27.12f;
+        long b = 987678965549L;
+        double c = 2.786;
+        short d = 569;
+        short e = -159;
+        int f = 27897;
+        byte g = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
     }
 }
